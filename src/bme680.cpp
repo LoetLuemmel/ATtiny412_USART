19a42663611b563d2cf78e4bedd83e46665f9749
Pit Förster <p.foerster@sealog.de>
2024-11-09 18:25:35 +0100

    Move BME680 functions to separate library
    - Created bme680.h and bme680.cpp
    - Moved all BME680-related functions to new files
    - Added calibration data structure
    - Implemented register read/write functions
    - Stored calibration values in global variable

--- src/bme680.cpp
+++ src/bme680.cpp
@@ -1,10 +1,54 @@
 #include "bme680.h"
 #include "twi.h"
 #include "uart.h"
+#include <util/delay.h>
 
 #define BME680_ADDR 0x76
 
-static struct bme680_calib calib;  // Globale Variable für Kalibrierungswerte
+static struct bme680_calib calib;
+
+bool bme680_init(void) {
+    // Soft Reset
+    bme680_write_register(0xE0, 0xB6);
+    _delay_ms(5);
+    
+    // Temperatur-Kalibrierung
+    uint8_t val_e9 = bme680_read_register(0xE9);
+    uint8_t val_e8 = bme680_read_register(0xE8);
+    calib.T1 = (val_e9 << 8) | val_e8;
+    
+    uint8_t val_8a = bme680_read_register(0x8A);
+    uint8_t val_8b = bme680_read_register(0x8B);
+    calib.T2 = (val_8b << 8) | val_8a;
+    
+    calib.T3 = (int8_t)bme680_read_register(0x8C);
+    
+    // ... weitere Kalibrierungswerte ...
+    
+    // Temperatur-Messung konfigurieren
+    bme680_write_register(0x74, 0x01);  // osrs_t = 1 (1x oversampling)
+    
+    return true;
+}
+
+// Temperatur messen (in °C * 100)
+int16_t bme680_read_temperature(void) {
+    // Messung starten
+    bme680_write_register(0x74, bme680_read_register(0x74) | 0x20);  // Forced mode
+    
+    // Warten bis Messung fertig
+    while(bme680_read_register(0x1D) & 0x20) {
+        _delay_ms(1);
+    }
+    
+    // ADC-Wert lesen
+    uint32_t adc_temp = bme680_read_register(0x22) << 12;
+    adc_temp |= bme680_read_register(0x23) << 4;
+    adc_temp |= bme680_read_register(0x24) >> 4;
+    
+    // Temperatur berechnen
+    return bme680_calc_temperature(adc_temp, &calib);
+}
 
 void bme680_write_register(uint8_t reg, uint8_t value) {
     twi_start(BME680_ADDR << 1);
@@ -23,39 +67,6 @@ uint8_t bme680_read_register(uint8_t reg) {
     twi_stop();
     twi_init();
     return value;
-}
-
-bool bme680_init(void) {
-    uart_send_string("Reading calibration...\r\n");
-    
-    // Temperatur-Kalibrierung
-    uint8_t val_e9 = bme680_read_register(0xE9);
-    uint8_t val_e8 = bme680_read_register(0xE8);
-    calib.T1 = (val_e9 << 8) | val_e8;
-    
-    uart_send_string("T1 = ");
-    uart_send_byte((calib.T1 >> 12) < 10 ? '0' + (calib.T1 >> 12) : 'A' + (calib.T1 >> 12) - 10);
-    uart_send_byte(((calib.T1 >> 8) & 0x0F) < 10 ? '0' + ((calib.T1 >> 8) & 0x0F) : 'A' + ((calib.T1 >> 8) & 0x0F) - 10);
-    uart_send_byte(((calib.T1 >> 4) & 0x0F) < 10 ? '0' + ((calib.T1 >> 4) & 0x0F) : 'A' + ((calib.T1 >> 4) & 0x0F) - 10);
-    uart_send_byte((calib.T1 & 0x0F) < 10 ? '0' + (calib.T1 & 0x0F) : 'A' + (calib.T1 & 0x0F) - 10);
-    uart_send_string("\r\n");
-    
-    uint8_t val_8a = bme680_read_register(0x8A);
-    uint8_t val_8b = bme680_read_register(0x8B);
-    calib.T2 = (val_8b << 8) | val_8a;
-    
-    uart_send_string("T2 = ");
-    if (calib.T2 < 0) {
-        uart_send_string("-");
-        calib.T2 = -calib.T2;
-    }
-    uart_send_byte((calib.T2 >> 12) < 10 ? '0' + (calib.T2 >> 12) : 'A' + (calib.T2 >> 12) - 10);
-    uart_send_byte(((calib.T2 >> 8) & 0x0F) < 10 ? '0' + ((calib.T2 >> 8) & 0x0F) : 'A' + ((calib.T2 >> 8) & 0x0F) - 10);
-    uart_send_byte(((calib.T2 >> 4) & 0x0F) < 10 ? '0' + ((calib.T2 >> 4) & 0x0F) : 'A' + ((calib.T2 >> 4) & 0x0F) - 10);
-    uart_send_byte((calib.T2 & 0x0F) < 10 ? '0' + (calib.T2 & 0x0F) : 'A' + (calib.T2 & 0x0F) - 10);
-    uart_send_string("\r\n");
-    
-    return true;
 }
 
 // Funktion zum Abrufen der Kalibrierungswerte
